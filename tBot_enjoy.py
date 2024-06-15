8d3f665d581d69e09bc5cdfc9b814977a5a1a32b
Classic-py <antonov5927@gmail.com>
2024-06-15 19:54:13 +0300

    commit: start_project

--- tBot_enjoy.py
+++ tBot_enjoy.py
@@ -1,2 +1,8 @@
-print('crap')
-print('boy')
+import telebot
+
+bot = telebot.TeleBot('7359683723:AAHVfnQvwvS1-vWEPQZ4e52_pZoaHTcQ7os')
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Здравствуйте, возникли проблемы с техникой?')
+
+bot.polling(none_stop=True)
