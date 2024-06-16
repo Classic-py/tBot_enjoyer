1fdc4fb8426dcf6501e248ea14ac1333f1c25093
Classic-py <antonov5927@gmail.com>
2024-06-16 20:42:12 +0300

    commit: new_theme

--- tBot_enjoy.py
+++ tBot_enjoy.py
@@ -1,8 +1,21 @@
 import telebot
 
-bot = telebot.TeleBot('7359683723:AAHVfnQvwvS1-vWEPQZ4e52_pZoaHTcQ7os')
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, 'Здравствуйте, возникли проблемы с техникой?')
+bot = telebot.TeleBot("7359683723:AAHVfnQvwvS1-vWEPQZ4e52_pZoaHTcQ7os")
 
-bot.polling(none_stop=True)
+
+@bot.message_handler(commands=["start", "help"])
+def commands(message):
+    bot.reply_to(message, "Использованна одна из двух команд")
+
+
+@bot.message_handler(content_types=["text", "photo", "sticker"])
+def content(message):
+    if message.text == "привет":
+        bot.send_message(message.chat.id, "Ну привет :3")
+    if message.photo:
+        bot.send_message(message.chat.id, "U so deutiful")
+    if message.sticker:
+        bot.send_message(message.chat.id, "Крутой стикер")
+
+
+bot.polling()
