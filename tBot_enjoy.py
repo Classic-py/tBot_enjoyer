39eecea455c956315f733241a1d89c443c0e396d
Classic-py <antonov5927@gmail.com>
2024-07-13 15:29:08 +0300

    echo bot

--- tBot_enjoy.py
+++ tBot_enjoy.py
@@ -1,10 +1,25 @@
-def founder(data, key):
-    if key in data:
-        return data[key]
-    else:
-        for value in data.values():
-            if type(value) == dict:
-                dict_lower = founder(value, key)
-                if dict_lower is not None:
-                    return dict_lower
-        
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+BOT_TOKEN = "7359683723:AAHVfnQvwvS1-vWEPQZ4e52_pZoaHTcQ7os"
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Салам алейкум, брат!\nЕсть работа для меня?')
+
+
+@dp.message(Command(commands=["help"]))
+async def process_help_command(message: Message):
+    await message.answer("Чем могу быть полезен?")
+
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply("С такими командами я пока что не знаком.\n" 
+                        "Попробуйте /start или /help")
+     
+if __name__ == '__main__':
+    dp.run_polling(bot)
