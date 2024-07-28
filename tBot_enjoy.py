73897ff929160ea31944d8090bcb77fdbd38cc76
Classic-py <antonov5927@gmail.com>
2024-07-28 16:31:36 +0300

    create random int

--- tBot_enjoy.py
+++ tBot_enjoy.py
@@ -1,25 +1,154 @@
-from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
+import json
+import random
+import requests
+
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
 
-BOT_TOKEN = "pass"
+BOT_TOKEN = "BOT_TOKEN"
+API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-@dp.message(Command(commands=["start"]))
+ATTEMPTS = 5
+
+user = {'in_game': False,
+        'secret_number': None,
+        'attempts': None,
+        'total_games': 0,
+        'wins': 0}
+
+def get_random_number() -> int:
+    return random.randint(1, 100)
+
+@dp.message(CommandStart())
 async def process_start_command(message: Message):
-    await message.answer('Салам алейкум, брат!\nЕсть работа для меня?')
+    await message.answer(
+        'Привет!\nДавайте сыграем в игру "Угадай число"?\n\n'
+        'Чтобы получить правила игры и список доступных '
+        'команд - отправьте команду /help'
+    )
 
-
-@dp.message(Command(commands=["help"]))
+@dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
-    await message.answer("Чем могу быть полезен?")
+    await message.answer(
+        f'Правила игры:\n\nЯ загадываю число от 1 до 100, '
+        f'а вам нужно его угадать\nУ вас есть {ATTEMPTS} '
+        f'попыток\n\nДоступные команды:\n/help - правила '
+        f'игры и список команд\n/cancel - выйти из игры\n'
+        f'/stat - посмотреть статистику\n/reset - обнулить' 
+        ' статистику\nДавай сыграем?'
+    )
+
+@dp.message(Command(commands='stat'))
+async def process_help_command(message: Message):
+    await message.answer(
+        f'Всего игр сыграно: {user["total_games"]}\n'
+        f'Игр выиграно: {user["wins"]}'
+    )
+
+@dp.message(Command(commands='reset'))
+async def process_reset_command(message: Message):
+    user['total_games'] = 0
+    user['wins'] = 0
+    await message.answer('Статистика обнулена.') 
+
+@dp.message(Command(commands='cancel'))
+async def process_cancel_command(message: Message):
+    if user['in_game']:
+        user['in_game'] = False
+        await message.answer(
+            'Вы вышли из игры. Если захотите сыграть '
+            'снова - напишите об этом'
+        )
+    else:
+        await message.answer(
+            'А мы и так с вами не играли :3'
+            'Может сыграем хоть разок?'
+        )
+
+@dp.message(F.text.lower().in_(['да', 'давай', 'сыграем', 'игра',
+                                'играть', 'хочу играть']))
+async def process_positive_answer(message: Message):
+    if not user['in_game']:
+        user['in_game'] = True
+        user['secret_number'] = get_random_number()
+        user['attempts'] = ATTEMPTS
+        await message.answer(
+            'Ура!\n\nЯ загадал число от 1 до 100, '
+            'попробуй угадать!'
+        )
+    else:
+        await message.answer(
+            'Пока мы играем в игру я могу '
+            'реагировать только на числа от 1 до 100 '
+            'и команды /cancel и /stat'
+        )
+
+@dp.message(F.text.lower().in_(['нет', 'не', 'не хочу', 'не буду']))
+async def process_negative_answer(message: Message):
+    if not user['in_game']:
+        await message.answer(
+            'Жаль :(\n\nЕсли захотите поиграть - просто '
+            'напишите об этом'
+        )
+    else:
+        await message.answer(
+            'Мы же сейчас с вами играем. Присылайте, '
+            'пожалуйста, числа от 1 до 100'
+        )
+
+@dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 100)
+async def process_numbers_answer(message: Message):
+    if user['in_game']:
+        if user['secret_number'] == int(message.text):
+            user['in_game'] = False
+            user['total_games'] += 1
+            user['wins'] += 1
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+            else:
+                cat_link = 'Сдесь должен был быть кот'
+            await message.answer(
+                'Ура!!! Вы угадали число!\n'
+                'Ваш приз - котейка :3'
+                )
+            await message.answer_photo(photo=cat_link)
+            await message.answer('Хотите сыграть ещё?')
+        elif user['secret_number'] < int(message.text):
+            user['attempts'] -= 1
+            await message.answer('Мое число меньше')
+        elif user['secret_number'] > int(message.text):
+            user['attempts'] -= 1
+            await message.answer('Мое число больше')
+
+        if user['attempts'] == 0:
+            user['in_game'] = False
+            user['total_games'] += 1
+            await message.answer(
+                f'К сожалению, у вас больше не осталось '
+                f'попыток. Вы проиграли \n\nМое число '
+                f'было {user["secret_number"]}\n\nДавайте '
+                f'сыграем еще?'
+            )
+    else:
+        await message.answer('Мы еще не играем. Хотите сыграть?')
 
 @dp.message()
-async def send_echo(message: Message):
-    await message.reply("С такими командами я пока что не знаком.\n" 
-                        "Попробуйте /start или /help")
-     
+async def process_other_answers(message: Message):
+    if user['in_game']:
+        await message.answer(
+            'Мы же сейчас с вами играем. '
+            'Присылайте, пожалуйста, числа от 1 до 100'
+        )
+    else:
+        await message.answer(
+            'Я довольно ограниченный бот, давайте '
+            'просто сыграем в игру?'
+        )
+
 if __name__ == '__main__':
     dp.run_polling(bot)
